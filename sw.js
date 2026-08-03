56c0767775a24036f3ffb1adcf1e5e3fe283258f
Aye150328 <ayelendesireeyebra@gmail.com>
2026-08-02 21:08:11 -0300

    Boton de suscripcion va directo al checkout (cart permalink)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='tw-v7';
+const C='tw-v8';
 self.addEventListener('install',function(e){self.skipWaiting();});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.map(function(x){return caches.delete(x);}));}));self.clients.claim();});
 self.addEventListener('fetch',function(e){
